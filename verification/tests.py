49173302b2218dcfe58185bdbe7169188748591b
Alexander Lyabah <a.lyabah@checkio.org>
2020-02-24 16:55:10 +0200

    first version

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,38 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [0],
+            "answer": 1,
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": [1],
+            "answer": 0,
+        },
+        {
+            "input": [10],
+            "answer": 1,
+        },
+        {
+            "input": [101],
+            "answer": 0,
+        },
+        {
+            "input": [245],
+            "answer": 0,
+        },
+        {
+            "input": [100100],
+            "answer": 2,
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [3456],
+            "answer": 0,
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": [100234],
+            "answer": 0,
         }
     ]
 }
